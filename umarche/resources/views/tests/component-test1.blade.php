5e5a99eab98761a93ef33f41a5e6ca825aaa3876
yasu37 <ykan4249@gmail.com>
2022-08-18 18:32:44 +0900

    変数

--- umarche/resources/views/tests/component-test1.blade.php
+++ umarche/resources/views/tests/component-test1.blade.php
@@ -3,5 +3,5 @@
     ヘッダー１
   </header>
 コンポーネント１
-  <x-tests.card title="タイトル1" content="本文1"></x-tests.card>
+  <x-tests.card title="タイトル1" content="本文1"  :message="$message"></x-tests.card>
 </x-tests.app>
