fac2f3529bf64a00f9d8cee5ec363463b7bdb88d
yasu37 <ykan4249@gmail.com>
2022-08-18 19:07:05 +0900

    属性バック

--- umarche/resources/views/tests/component-test1.blade.php
+++ umarche/resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
 コンポーネント１
   <x-tests.card title="タイトル1" content="本文1"  :message="$message"></x-tests.card>
   <x-tests.card title="タイトル2"></x-tests.card>
+  <x-tests.card title="タイトル3" class="bg-red-300"></x-tests.card>
 </x-tests.app>
